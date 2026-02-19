ff6635d7e5c51d3aba032b528b125fe4fb5f6a41
senpai0002 <rimilhans2003@gmail.com>
2026-02-19 10:44:50 +0530

    Added file

--- diamond.cpp
+++ diamond.cpp
@@ -5,37 +5,35 @@ int main(){
     int n;
     cout<<"Enter value: ";
     cin>>n;
+
     n=(n+1)/2;
 
+    // First half
     for(int i = 1; i<=n; i++){
         for(int j = 1;j<=n-i;j++){
-            cout<<"  ";
+            cout<<"   ";
         }
         for(int j = 1;j<=2*i-1;j++){
             if(j==1 || j==2*i-1){
-                cout<<"* ";
+                cout<<"*  ";
             }
             else{
-                cout<<"  ";
+                cout<<"   ";
             }
         }
         cout<<endl;
     }
-    int k = n;
-    if (n%2==0){
-        k = k-1;
-    }
 
-    for(int i = k; i>=1; i--){
+    for(int i = n-1; i>=1; i--){
         for(int j =1; j<=n-i;j++){
-            cout<<"  ";
+            cout<<"   ";
         }
         for(int j = 1;j<=2*i-1;j++){
             if(j==1 || j==2*i-1){
-                cout<<"* ";
+                cout<<"*  ";
             }
             else{
-                cout<<"  ";
+                cout<<"   ";
             }
         }
         cout<<endl;
